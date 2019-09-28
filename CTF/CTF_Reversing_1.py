1ea2ff88f8ab2366aa269c401040b4fcfcfd60b9
Compl3x <54823961+compl3xo@users.noreply.github.com>
2019-09-28 00:46:38 -0400

    Create CTF_Reversing_1.py

---
+++ CTF/CTF_Reversing_1.py
@@ -0,0 +1,21 @@
+# python3
+# CTF By Compl3x!
+print("-----------------------\n[---] CTF by Beny [---]\n-----------------------")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+print(numbers[::2])
+db = '\x63'
+ga = [0x6E, 0x6F, 0x70, 0x65]
+flag = '\6C'
+lf = [0x6C, 0x6F, 0x6C]
+wut = '\6F'
+er = [0x6B,0x63, 0x61, 0x72, 0x63, 0x74, 0x69, 0x6F, 0x6C, 0x70, 0x78, 0x65]
+noflag = '\x73'
+eh = [0x6E, 0x6F, 0x20, 0x66, 0x6C, 0x61, 0x67, 0x20, 0x68, 0x65, 0x72, 0x65, 0x0D, 0x0A]
+stop = '\x65'
+x = ['F7', 'X', 'L1', 'G', '4E', 'H','G4', 'B', ':']
+y = ['a', 'b', 't', 'd', '1', 'f', '0', 'h', 'l', 'j', 'p', 'l', 'x', 'n', '7e', 'q', 'k', 'a', 'c', 'y', '4', 'u', 'R2', 'x', 'c']
+for i in range(0, len(x), 2):
+    print(x[i])  
+      
+for i in range(0, len(y), 2):
+    print(y[i])
